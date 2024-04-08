53f05e0b9dcb253bbb215718318f191075ce5af6
Juan Salles <juanprazeres@gmail.com>
2024-04-07 21:07:58 -0300

    Create(): Criando esqueleto do projeto e seus componentes base

--- PortifolioJuanSalles/src/app/app.module.ts
+++ PortifolioJuanSalles/src/app/app.module.ts
@@ -2,10 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import { SkillsComponent } from './Components/skills/skills.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { ExperienceComponent } from './Components/experience/experience.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { SkillCardComponent } from './Components/skill-card/skill-card.component';
+import { ProjectsComponent } from './Components/projects/projects.component';
+import { ProjectCardComponent } from './Components/project-card/project-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    SkillsComponent,
+    ContactComponent,
+    ExperienceComponent,
+    FooterComponent,
+    SkillCardComponent,
+    ProjectsComponent,
+    ProjectCardComponent
   ],
   imports: [
     BrowserModule
