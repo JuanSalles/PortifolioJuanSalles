d6f34f7f03354077d4add68b581a5f476ee2b391
Juan Salles <juanprazeres@gmail.com>
2024-04-22 21:26:59 -0300

    Ajuste do banner e resposividade da pagina

--- PortifolioJuanSalles/src/app/Components/about/about.component.ts
+++ PortifolioJuanSalles/src/app/Components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TypeWindown } from 'src/Interfaces/TypeWindown';
 
 @Component({
   selector: 'app-about',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
-
+  typeContentOne : TypeWindown = TypeWindown.image;
+  contentOne : string[] = [
+    '../../../assets/About/Foto-Perfil.png',
+    'Foto de perfil de Juan Salles'
+  ];
+  typeContentTwo : TypeWindown = TypeWindown.text;
+  contentTwo : string[] = [
+    'Olá.\nMe chamo Juan. Trabalho há mais de 10 anos com tecnologia. Meu primeiro  contato com programação foi em 2011, durante meu primeiro ano no ensino  médio técnico no Instituto Federal de Ciência e Tecnologia da Bahia  (IFBA ou CEFET para os mais íntimos).',
+    'Após me formar em 2015, consegui uma posição como  técnico e atuei no ramo operacional de eletrônica e televisão. Sempre  tive o desejo de continuar aprendendo programação, o que me levou, em  2021, a investir meu tempo de estudo na área e me enxergar como  desenvolvedor.',
+    'Comecei meus estudos com Javascript e seus frameworks no frontend e,  posteriormente, conquistei minha primeira vaga como Analista de Sistemas  na mesma empresa em que trabalhava como técnico (onde continuo até  hoje). Com essa oportunidade, pude adentrar no mundo do backend e agora  consigo atuar como fullstack, oferecendo suporte aos sistemas da empresa  e desenvolvendo novas aplicações com diversas tecnologias.',
+    'Atualmente, estou focado em aprender novas tecnologias e aprimorar minhas  habilidades de desenvolvimento, sempre buscando novos desafios e  oportunidades para crescer profissionalmente.'
+  ];
 }
